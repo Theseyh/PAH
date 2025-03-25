14af82ff8439f5a28cb90e69038fb7d3666b19ba
theseyh <leclercqlo@cy-tech.fr>
2025-03-25 11:24:23 +0100

    4

--- hello_world.cu
+++ hello_world.cu
@@ -4,13 +4,12 @@
 //./hello_world
 
 // CUDA kernel function
-__global__ void kernel() {
-    printf("Hello from the GPU!\n");
+__global__ void kernel() { 
 }
 
 int main(void) {
     // Launch the kernel with 1 block and 1 thread
-    kernel<<<1, 1>>>();
+    kernel<<<5, 1>>>();
 
     // Wait for the GPU to finish before printing from the CPU
     cudaDeviceSynchronize();
